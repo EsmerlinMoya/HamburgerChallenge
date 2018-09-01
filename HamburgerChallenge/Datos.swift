a78a7aa629528441250b60cebd686ce2cec4d660
Esmerlin Moya <esmerlinmoya@gmail.com>
2018-09-01 12:12:28 -0400

    first commit

---
+++ HamburgerChallenge/Datos.swift
@@ -0,0 +1,13 @@
+//
+//  Datos.swift
+//  HamburgerChallenge
+//
+//  Created by merlin Moya on 9/1/18.
+//  Copyright © 2018 merlin Moya. All rights reserved.
+//
+
+import UIKit
+
+class Datos: NSObject {
+
+}
